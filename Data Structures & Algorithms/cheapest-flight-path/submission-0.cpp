b394378a22c629b22ea0db885755daad9747ec0b
Muskan kumari <99859811+MuskanKumari2023@users.noreply.github.com>
2026-09-14 12:50:02 +0530

    Add: cheapest-flight-path - submission-0

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-0.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+vector<vector<pair<int,int>>>g;
+vector<int>vis;
+vector<vector<int>>dist;
+void dykstra(int src,int dst,int k){
+    dist[src][0]=0;
+    priority_queue<pair<int,pair<int,int>>>pq;
+    pq.push({0,{src,0}});
+    while(!pq.empty()){
+        auto cur=pq.top();
+        pq.pop();
+        int curd=-cur.first;
+        int curn=cur.second.first;
+        int cnt=cur.second.second;
+        // if(vis[curn])continue;
+        // vis[curn]=1;
+        if(cnt>k)continue ;
+        for(auto v:g[curn]){
+            if(dist[v.first][cnt+1]>dist[curn][cnt]+v.second){
+                dist[v.first][cnt+1]=dist[curn][cnt]+v.second;
+                pq.push({-dist[v.first][cnt+1],{v.first,cnt+1}});
+            }
+        }
+    }
+}
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        g.resize(n);
+        vis.assign(n,0);
+        dist.assign(n,vector<int>(k+2,1e9));
+        for(auto &x:flights){
+            int u=x[0];
+            int v=x[1];
+            int w=x[2];
+            g[u].push_back({v,w});
+        }
+        dykstra(src,dst,k);
+        int ans=1e9;
+        for(int i=0;i<=k+1;i++){
+            ans=min(ans,dist[dst][i]);
+        }
+        if(ans>=1e9)return -1;
+        return ans;
+    }
+};
